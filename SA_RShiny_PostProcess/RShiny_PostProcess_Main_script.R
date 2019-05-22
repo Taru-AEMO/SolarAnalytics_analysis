008d6625d8dcf63f469c368a7b663a1252b3f52f
Rachel <47378487+rachelet@users.noreply.github.com>
2019-05-22 09:07:34 +0800

    Merge branch 'master' of https://github.com/Taru-AEMO/SolarAnalytics_analysis

--- SA_RShiny_PostProcess/RShiny_PostProcess_Main_script.R
+++ SA_RShiny_PostProcess/RShiny_PostProcess_Main_script.R
@@ -17,12 +17,13 @@ setwd(paste0("",directory,""))
 ################# set inputs
 ## save short underlying data from r shiny ______ this file is used for all plots of minutes/hour near event
 # underlying_data_file <- "E1_6_short_ud.csv"
-# underlying_data_file <- "E2_13_short_ud.csv"
+underlying_data_file <- "E2_13_short_ud.csv"
 # underlying_data_file <- "E3_10_short_ud.csv"
 # underlying_data_file <- "E4_5_short_ud.csv"
 
-underlying_data_file <- "T1_2_short_ud.csv"
-underlying_data_file <- "T2_2_short_ud.csv"
+# underlying_data_file <- "T1_2_short_ud.csv"
+# underlying_data_file <- "T2_2_short_ud.csv"
+
 
 
 ## save long underlying data from r shiny ______ this file is used for one plot and must include time series for entire day
@@ -30,10 +31,11 @@ underlying_data_file <- "T2_2_short_ud.csv"
 
 ## toggle upscaled data and save aggregated results from r shiny
 # upscaled_aggregated_file <- "E1_6_short_upscaled_ar.csv"
-# upscaled_aggregated_file <- "E2_13_short_upscaled_ar.csv"
+upscaled_aggregated_file <- "E2_13_short_upscaled_ar.csv"
 # upscaled_aggregated_file <- "E3_10_short_upscaled_ar.csv"
 # upscaled_aggregated_file <- "E4_5_short_upscaled_ar.csv"
-upscaled_aggregated_file <- NULL
+# upscaled_aggregated_file <- NULL
+
 
 ####### event data, only one t0 can be input per analysis. multiple events can be input.
 ###### don't deviate from formats
@@ -41,28 +43,28 @@ event_date <- "20180415"
 
 ### t0 is the interval immediately prior to event <-- must match exact time stamp in data
 # pre_event_interval <- "07:52:25"
-# pre_event_interval <- "09:29:25"
+pre_event_interval <- "09:29:25"
 # pre_event_interval <- "09:43:55"
 # pre_event_interval <- "11:29:55"
 
 # pre_event_interval <- "09:29:55"
-pre_event_interval <- "09:32:55"
-pre_event_interval <- "09:35:55"
-pre_event_interval <- "09:36:55"
-pre_event_interval <- "09:38:55"
-pre_event_interval <- "09:43:55"
-pre_event_interval <- "09:49:55"
+# pre_event_interval <- "09:32:55"
+# pre_event_interval <- "09:35:55"
+# pre_event_interval <- "09:36:55"
+# pre_event_interval <- "09:38:55"
+# pre_event_interval <- "09:43:55"
+# pre_event_interval <- "09:49:55"
 
 
 # event_time <- c("07:52:55","07:53:55")
-# event_time <- c("09:31:25","09:33:55","09:34:25","09:36:55","09:37:55","09:38:25","09:39:55")
+event_time <- c("09:31:25","09:33:55","09:34:25","09:36:55","09:37:55","09:38:25","09:39:55")
 # event_time <- c("09:44:25","09:45:55","09:50:55")
 # event_time <- c("11:30:55")
 
 # event_time <- c("09:31:25")
-event_time <- c("09:33:25","09:33:55")
-event_time <- c("09:36:25")
-event_time <- c("09:37:25")
+# event_time <- c("09:33:25","09:33:55")
+# event_time <- c("09:36:25")
+# event_time <- c("09:37:25")
 
 
 ######
@@ -86,6 +88,11 @@ source("RShiny_PostProcess_UpscaledTables.R")
 ############# data analysis - plots
 setwd("~/GitHub/DER_Event_analysis/SolarAnalytics_analysis/SA_RShiny_PostProcess")
 source("RShiny_PostProcess_Plots.R")
+
+############ upscaled Plots
+setwd("~/GitHub/DER_Event_analysis/SolarAnalytics_analysis/SA_RShiny_PostProcess")
+source("RShiny_PostProcess_UpscaledPlots.R")
+
 
 #######################################################################
 
--- SA_RShiny_PostProcess/RShiny_PostProcess_UpscaledTables.R
+++ SA_RShiny_PostProcess/RShiny_PostProcess_UpscaledTables.R
@@ -4,7 +4,95 @@
 
 ##### 1. create tables ####
 
+power_preevent <- upscaled_ts %>% 
+  filter(ts==t0) 
+names(power_preevent)[names(power_preevent)=="MW_upscaled"] <- "PreEvent_MW"
 
+#Find Minimum Power following the event
+power_event <- upscaled_ts %>% 
+  filter(ts%in%tx)
+names(power_event)[names(power_event)=="MW_upscaled"] <- "Event_MW"
+
+
+#Calculate the Power Loss during the event by Tranch
+temp.df = left_join(power_preevent, power_event, by=c("response_category", "Standard_Version")) 
+
+df_total <- temp.df %>% 
+  group_by(ts.x) %>% 
+  summarise(PreEvent_MW = sum(PreEvent_MW),
+            Event_MW = sum(Event_MW)) %>% 
+  mutate(Tot_Power_Loss_MW = PreEvent_MW-Event_MW) %>% 
+  mutate(Tot_ChangeInPower_perc = Tot_Power_Loss_MW/PreEvent_MW)
+
+temp_df_total <- df_total %>% 
+  select(ts.x, Tot_Power_Loss_MW)
+  
+
+#Evaluate Power loss by Tanch and response category
+df_tranch_response <- temp.df %>%   
+  mutate(Power_Loss_MW = PreEvent_MW-Event_MW) %>% 
+  mutate(ChangeInPower_perc = Power_Loss_MW/PreEvent_MW) %>% 
+  left_join(.,temp_df_total, by="ts.x") %>% 
+  mutate(Proportion_DeltaP_perc = Power_Loss_MW/Tot_Power_Loss_MW) %>% 
+  select(ts.x, response_category, Standard_Version, PreEvent_MW, Event_MW,Power_Loss_MW, ChangeInPower_perc, Proportion_DeltaP_perc)
+
+
+
+#Evaluate Power loss by response category
+df_response <- temp.df %>% 
+  group_by(ts.x, response_category) %>% 
+  summarise(PreEvent_MW = sum(PreEvent_MW),
+            Event_MW = sum(Event_MW)) %>% 
+  mutate(Power_Loss_MW = PreEvent_MW-Event_MW) %>% 
+  mutate(ChangeInPower_perc = Power_Loss_MW/PreEvent_MW) %>% 
+  left_join(.,temp_df_total, by="ts.x") %>% 
+  mutate(Proportion_DeltaP_perc = Power_Loss_MW/Tot_Power_Loss_MW) %>% 
+  select(ts.x, response_category, PreEvent_MW, Event_MW,Power_Loss_MW, ChangeInPower_perc,Proportion_DeltaP_perc)
+
+
+
+
+
+#Evaluate Power loss by response category
+df_tranch <- temp.df %>% 
+  group_by(ts.x, Standard_Version) %>% 
+  summarise(PreEvent_MW = sum(PreEvent_MW),
+            Event_MW = sum(Event_MW)) %>% 
+  mutate(Power_Loss_MW = PreEvent_MW-Event_MW) %>% 
+  mutate(ChangeInPower_perc = Power_Loss_MW/PreEvent_MW) %>% 
+  left_join(.,temp_df_total, by="ts.x") %>% 
+  mutate(Proportion_DeltaP_perc = Power_Loss_MW/Tot_Power_Loss_MW) %>% 
+  select(ts.x, Standard_Version, PreEvent_MW, Event_MW,Power_Loss_MW, ChangeInPower_perc,Proportion_DeltaP_perc)
+
+
+
+##### save outputs ####
+setwd(paste0("",directory,"/PP_output_",event_date,""))
+
+sink("Power_Loss_Upscale.csv")
+cat("Total Power Loss")
+cat('\n')
+write.csv(df_total)
+cat('____________________________')
+cat('\n')
+cat('\n')
+cat("Power Loss By Tranch")
+cat('\n')
+write.csv(df_tranch)
+cat('____________________________')
+cat('\n')
+cat('\n')
+cat("Power Loss By Response")
+cat('\n')
+write.csv(df_response)
+cat('____________________________')
+cat('\n')
+cat('\n')
+cat("Power Loss By Tranch and Response")
+cat('\n')
+write.csv(df_tranch_response)
+cat('____________________________')
+sink()
 
 
 
