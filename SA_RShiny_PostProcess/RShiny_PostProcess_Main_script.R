bbb641206d9e1fcf4a9752a91bf64575f9d7eb7e
Taru <taru.veijalainen@aemo.com.au>
2019-05-24 13:22:35 +1000

    Main Script - just commented out event details

--- SA_RShiny_PostProcess/RShiny_PostProcess_Main_script.R
+++ SA_RShiny_PostProcess/RShiny_PostProcess_Main_script.R
@@ -10,14 +10,14 @@ library("gridExtra")
 
 
 ################ set directory for all inputs / outputs
-directory <- "~/DER_disturbance_analysis/NSW_BF_Results"
+directory <- "~/GitHub/DER_Event_analysis/SolarAnalytics_analysis/output/All_Event_Results"
 
 #
 setwd(paste0("",directory,""))
 ################# set inputs
 ## save short underlying data from r shiny ______ this file is used for all plots of minutes/hour near event
 # underlying_data_file <- "E1_6_short_ud.csv"
-underlying_data_file <- "E2_13_short_ud.csv"
+#underlying_data_file <- "20180118_underlying.csv"
 # underlying_data_file <- "E3_10_short_ud.csv"
 # underlying_data_file <- "E4_5_short_ud.csv"
 
@@ -30,7 +30,7 @@ underlying_data_file <- "E2_13_short_ud.csv"
 
 ## toggle upscaled data and save aggregated results from r shiny
 # upscaled_aggregated_file <- "E1_6_short_upscaled_ar.csv"
-upscaled_aggregated_file <- "E2_13_short_upscaled_ar.csv"
+#upscaled_aggregated_file <- "20180118_upscale.csv"
 # upscaled_aggregated_file <- "E3_10_short_upscaled_ar.csv"
 # upscaled_aggregated_file <- "E4_5_short_upscaled_ar.csv"
 # upscaled_aggregated_file <- NULL
@@ -39,11 +39,11 @@ upscaled_aggregated_file <- "E2_13_short_upscaled_ar.csv"
 
 ####### event data, only one t0 can be input per analysis. multiple events can be input.
 ###### don't deviate from formats
-event_date <- "20180415"
+#event_date <- "20180118"
 
 ### t0 is the interval immediately prior to event <-- must match exact time stamp in data
 # pre_event_interval <- "07:52:25"
-pre_event_interval <- "09:29:25"
+#pre_event_interval <- "15:18:25"
 # pre_event_interval <- "09:43:55"
 # pre_event_interval <- "11:29:55"
 
@@ -57,7 +57,7 @@ pre_event_interval <- "09:29:25"
 
 
 # event_time <- c("07:52:55","07:53:55")
-event_time <- c("09:31:25","09:33:55","09:34:25","09:36:55","09:37:55","09:38:25","09:39:55")
+#event_time <- c( "15:19:25")
 # event_time <- c("09:44:25","09:45:55","09:50:55")
 # event_time <- c("11:30:55")
 
