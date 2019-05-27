dfdb726b6c2214543b3c74a6e50a629e0f930a9d
Etaylor-AEMO <eloise.taylor@aemo.com.au>
2019-05-27 16:41:47 +1000

    Update RShiny_PostProcess_RawTables.R
    Added in table 5: sample count (cleanded data only)

--- SA_RShiny_PostProcess/RShiny_PostProcess_RawTables.R
+++ SA_RShiny_PostProcess/RShiny_PostProcess_RawTables.R
@@ -231,11 +231,22 @@ table_4 <- temp.join %>%
 
 rm(list=ls(pattern="temp"))
 
-####ELOISE START HERE###
-##Start with the pp_ud Dataframe :)
+#### table.5: Unscaled data: sample count, cleaned data only ####
 
+#Create basis for table 5, grouping by Standard and Size
+temp.table1 <- unique_list %>%
+  group_by(Standard_Version,Grouping) %>%
+  summarise(n=n_distinct(c_id)) %>%
+  spread(Grouping,n) 
 
+#Add row totals
+temp.table1$Total = rowSums( temp.table1[ sapply(temp.table1, is.numeric)] )
 
+#Add column totals and create final table
+table_5 <- rbind( temp.table1, append(c(Standard_Version="Total"),  
+                   as.list(colSums( temp.table1[ sapply(temp.table1, is.numeric)] ))))
+
+rm(list=ls(pattern="temp"))
 
 ##### 3 save outputs ####
 setwd(paste0("",directory,"/PP_output_",event_date,""))
@@ -245,6 +256,7 @@ write.csv(table_1,file=paste0("table1_",savetime,".csv"))
 write.csv(table_2,file=paste0("table2_",savetime,".csv"))
 write.csv(table_3,file=paste0("table3_",savetime,".csv"))
 write.csv(table_4,file=paste0("table4_",savetime,".csv"))
+write.csv(table_5,file=paste0("table5_",savetime,".csv"))
 
 
 rm(list=ls(pattern="table"))
